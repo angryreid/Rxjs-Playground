7b79ba500d6c11dc8476302f3f998fd785444327
angryreid <onyourwill@gmail.com>
2022-05-29 21:19:45 +0800

    1 file modified

--- index.ts
+++ index.ts
@@ -1,20 +1,20 @@
-import { merge, interval } from 'rxjs'; 
+import { merge, interval } from 'rxjs';
 import { map, take } from 'rxjs/operators';
 import { timespan } from './operators/timespan';
-
-
 
 // test use case
 
 const slowSource = interval(2000).pipe(
-  map(i => `Slow message nº ${i}`),
+  map((i) => `Slow message nº ${i}`),
   take(4)
 );
 const quickSource = interval(300).pipe(
-  map(i => `Quick message nº ${i}`),
+  map((i) => `Quick message nº ${i}`),
   take(10)
 );
 
+console.log('testing in stackblitz');
+
 const source = merge(slowSource, quickSource);
 
-source.pipe(timespan(2000)).subscribe(x => console.log(x));
+source.pipe(timespan(2000)).subscribe((x) => console.log(x));
